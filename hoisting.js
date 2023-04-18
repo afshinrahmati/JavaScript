358ddabbf62129743e33fd16076809a053f9b70d
a.rahmati <a.rahmati@kianiranian.com>
2023-04-18 13:23:01 +0330

    learn: hoisting

---
+++ hoisting.js
@@ -0,0 +1,21 @@
+// that mean is up
+// Before js run my code all variable and function
+// changing automaticly like standard
+simle();
+
+function smile() {
+  console.log("test");
+}
+
+// var
+
+console.log(name);
+var name = "afshin";
+// it log undefine becases variable name is exsits but dont print on there any thing
+// ###
+var name;
+console.log(name);
+name = "afshin";
+
+// let & const
+// two this create for this that you should first varibales then use
