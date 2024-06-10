da38e425f86b9e0dc80835ec9cac1960e93c0378
afshin <afshinrahmati93@gmail.com>
2024-06-10 10:07:43 +0330

    feat: :art: test

---
+++ part-two/function-invocation.js
@@ -0,0 +1,12 @@
+// Function invocation is the process of executing a function. It is also
+// known as calling a function. To invoke a function, you simply need to write the function name followed
+// by a pair of parentheses. You can also pass arguments to the function by writing them inside
+// the parentheses
+
+
+const canada = function() {
+    console.log("the weather is so cold")
+}
+function india() {
+    console.log("the weather is too warm")
+}
