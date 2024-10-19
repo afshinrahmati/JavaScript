a37ebe40b9da2b0b64e3b7db0eed854213a2876a
afshinrahmati <afshinrahmati93@gmail.com>
2024-10-19 18:58:14 +0330

    feat: :art: what is dynamic and stastic type

---
+++ javascript/part-three/static-daynamic.js
@@ -0,0 +1,9 @@
+// static-type ==> ensuring that variables are used with the correct types.is done at compile time rather than at runtime
+// Example Languages: C, C++, Java, Go, Rust.
+// Type Checking: In static programming, type checking occurs during compilation, while in dynamic programming, it happens during runtime.
+//  Dynamic Programming (Dynamically Typed Languages):
+// Dynamic programming refers to the use of dynamically typed languages, where type checking is performed at runtime.
+// Example Languages: Python, JavaScript, Ruby, PHP.
+
+number = 5       // # number is an int
+number = "Hello"  //# now number is a string, no issue at runtime
